a6403acad4c0955d16fe7c451f58fc2b64d6c911
javadi <javadi89s@gmail.com>
2025-09-08 11:34:10 +0330

    feat: :sparkles: adding login to navbar after sign out

--- src/components/header/profile/Profile.tsx
+++ src/components/header/profile/Profile.tsx
@@ -4,71 +4,84 @@ import NavbarMobile from "../navbar/NavbarMobile";
 import useLogout from "@/hooks/use-logout";
 import { NavLink } from "react-router-dom";
 import { ProfileItems } from "./ProfileItems";
+import useAuthStore from "@/store/use-auth-store";
 
-const Profile: React.FC = () => {
+const Profile = () => {
   const [profileOpen, setProfileOpen] = useState<boolean>(false);
-  const logout = useLogout(); 
+  const logout = useLogout();
+  const { id } = useAuthStore();
 
   const handleClick = (title: string) => {
     if (title === "Sign out") {
-      logout.mutate(); 
+      logout.mutate();
     }
-    setProfileOpen(false); 
+    setProfileOpen(false);
   };
 
   return (
     <nav className="">
       <div className="hidden md:block">
-        <div className="ml-4 flex items-center md:ml-6">
+        <div className="ml-4 flex items-center gap-2 md:ml-6">
           <Notification />
-          <div className="relative ml-3">
-            <button
-              onClick={() => setProfileOpen(!profileOpen)}
-              type="button"
-              className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              id="user-menu-button"
-              aria-expanded={profileOpen}
-              aria-haspopup="true"
-            >
-              <span className="sr-only">Open user menu</span>
-              <img
-                className="h-8 w-8 rounded-full"
-                src="https://cdn.jsdelivr.net/gh/alohe/memojis/png/vibrent_6.png"
-                alt="Profile"
-              />
-            </button>
-            {profileOpen && (
-              <div
-                className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu-button"
+
+          {id ? (
+            <div className="relative ml-3">
+              <button
+                onClick={() => setProfileOpen(!profileOpen)}
+                type="button"
+                className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                id="user-menu-button"
+                aria-expanded={profileOpen}
+                aria-haspopup="true"
               >
-                {ProfileItems.map((item) => (
-                  <div key={item.title}>
-                    {item.title === "Sign out" ? (
-                      <button
-                      className="flex w-full items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                      onClick={() => handleClick(item.title)}
-                      >
-                        {item.icon}
-                        {item.title}
-                      </button>
-                    ) : (
-                      <NavLink
-                        to={item.address}
-                        className="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        onClick={() => handleClick(item.title)}
-                      >
-                        {item.icon}
-                        {item.title}
-                      </NavLink>
-                    )}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
+                <span className="sr-only">Open user menu</span>
+                <img
+                  className="h-8 w-8 rounded-full"
+                  src="https://cdn.jsdelivr.net/gh/alohe/memojis/png/vibrent_6.png"
+                  alt="Profile"
+                />
+              </button>
+
+              {profileOpen && (
+                <div
+                  className="absolute right-0 z-10 mt-2 w-36 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="user-menu-button"
+                >
+                  {ProfileItems.map((item) => (
+                    <div key={item.title}>
+                      {item.title === "Sign out" ? (
+                        <button
+                          className="flex w-full items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          onClick={() => handleClick(item.title)}
+                        >
+                          {item.icon}
+                          {item.title}
+                        </button>
+                      ) : (
+                        <NavLink
+                          to={item.address}
+                          className="flex items-center gap-2 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                          onClick={() => handleClick(item.title)}
+                        >
+                          {item.icon}
+                          {item.title}
+                        </NavLink>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          ) : (
+            <NavLink
+              to="auth?mode=login"
+              className="rounded-md bg-indigo-500 px-3 py-1 text-sm font-medium text-white hover:bg-indigo-700"
+            >
+              Log in
+            </NavLink>
+          )}
         </div>
       </div>
       <NavbarMobile />
--- src/hooks/use-logout.ts
+++ src/hooks/use-logout.ts
@@ -1,15 +1,18 @@
-import { axiosInstance } from "@/lib/utils";
 import useAuthStore from "@/store/use-auth-store";
 import { useMutation } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const useLogout = () => {
+  const navigate = useNavigate();
   const reset = useAuthStore((state) => state.reset);
 
   return useMutation({
-    mutationFn: () => axiosInstance.post("/users/logout"),
-    onSuccess: () => {
+    mutationFn: async () => {
       reset();
+    },
+    onSuccess: () => {
+      navigate("/auth?mode=login");
       toast.success("You logged out successfully");
     },
     onError: () => {
